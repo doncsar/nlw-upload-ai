b38d756ae7ea1312a63ec3ddb212c23faaff7721
César HC Carvalho <cesa.h.carvalho@gmail.com>
2023-09-13 06:58:59 -0300

    setup gh-pages

---
+++ .history/src/App_20230913065034.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <h1>
+     hello world
+      </h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam corrupti repellat aliquid voluptate, explicabo consequuntur amet animi libero, vel et reprehenderit repellendus inventore, qui adipisci fugit corporis excepturi aut laboriosam.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ .history/src/App_20230913065044.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <h1>
+     hello world
+      </h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam corrupti repellat aliquid voluptate, explicabo consequuntur amet animi libero, vel et reprehenderit repellendus inventore, qui adipisci fugit corporis excepturi aut laboriosam.</p>
+    </div>
+  )
+}
+
+export default App
